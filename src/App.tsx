c54202ed8418b076ef7bb14d7a45dc3790d6fdb1
rutiini <rutiluz88@gmail.com>
2019-03-29 20:40:15 +0200

    cleaned up the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { createStyles, WithStyles, withStyles } from '@material-ui/core';
-import React, { FunctionComponent, useReducer } from 'react';
-import { IAppContext, mutateState } from './Components/AppContext';
+import React, { FunctionComponent, useReducer, useState } from 'react';
+import { save } from './Components/AppContext';
 import ExerciseContext from './Components/AppReducer/ExerciseContext';
 import { ActionType, DefaultAppState, ExerciseReducer } from './Components/AppReducer/ExerciseReducer';
 import Clock from './Components/Clock';
@@ -9,7 +9,7 @@ import EditExerciseDialog from './Components/dialogs/EditExerciseDialog';
 import EditSectionDialog from './Components/dialogs/EditSectionDialog';
 import NotificationSnackBar from './Components/NotificationSnackBar';
 import BottomNavTabs from './Components/tabs/BottomNavTabs';
-import { IExercise, ISection } from './DataInterfaces';
+import { IExercise } from './DataInterfaces';
 
 interface IProps extends WithStyles<typeof styles> {
   // just for style injection
@@ -24,9 +24,10 @@ const styles = createStyles({
 /** TODO: refactor using hooks, replace context with reducer */
 const App: FunctionComponent<IProps & WithStyles> = (props: IProps) => {
 
+  const [confirmOpen, setConfirmOpen] = useState(false);
   const [state, dispatch] = useReducer(ExerciseReducer, DefaultAppState);
   const { classes } = props;
-  const { editSection, editExercise } = state;
+  const { editSection, editExercise } = state
 
   const validateExerciseName = (name: string) => {
 
@@ -39,53 +40,25 @@ const App: FunctionComponent<IProps & WithStyles> = (props: IProps) => {
     }
   }
   const deleteExercise = (exercise: IExercise) => dispatch({ type: ActionType.DeleteExercise, payload: exercise });
-  /** provide the reducer through context during transformation, after hooks are fully implemented the context becomes obsolete */
-  // Refactor as separate functions (only necessary ones should be declared here, not everything.)
-  const contextValue: IAppContext = {
-    dispatch: {
-      addExercise: (exercise: IExercise) => dispatch({ type: ActionType.AddExercise, payload: exercise }),
-      addSection: (section: ISection) => dispatch({ type: ActionType.AddSection, payload: section }),
-      deleteExercise: (exercise: IExercise) => { dispatch({ type: ActionType.DeleteExercise, payload: exercise }); return true; },
-      deleteSection: (section: ISection) => { dispatch({ type: ActionType.DeleteSection, payload: section }); return true; },
-      mutateState,
-      save: () => void 0, // TODO: rewrite localstorage handling.
-      setActiveSection: (section: ISection) => dispatch({ type: ActionType.SetActiveSection, payload: section }),
-      setConfirmOpen: () => void 0,
-      setEditExercise: (exercise: IExercise) => dispatch({ type: ActionType.SetEditExercise, payload: exercise }),
-      setEditSection: (section: ISection) => dispatch({ type: ActionType.SetEditSection, payload: section }),
-      setExercises: (allExercises: ReadonlyArray<IExercise>) => dispatch({ type: ActionType.UpdateAllExercises, payload: allExercises }),
-      setSelectedExercise: (exercise: IExercise) => dispatch({ type: ActionType.SetActiveExercise, payload: exercise }),
-      setSelectedSection: (section: ISection) => dispatch({ type: ActionType.SetEditSection, payload: section }),
-      setTime: (time: Date) => dispatch({ type: ActionType.UpdateStartTime, payload: time }),
-      showMessage: () => void 0,
-      updateSection: (section: ISection) => dispatch({ type: ActionType.UpdateSection, payload: section }),
-      updateSectionOrder: (sections: ReadonlyArray<ISection>) => dispatch({ type: ActionType.UpdateAllSections, payload: sections }),
-    },
-    state: {
-      ...state,
-      selectedExercise: state.activeExercise // to be removed
-    }
-  }
 
   return (
-    // <AppContext.Provider value={contextValue}>
-      <ExerciseContext.Provider value={[state, dispatch]}>
-        <div className={classes.App}>
-          <Clock canvasSide={100} />
-          <BottomNavTabs />
-          {editSection && <EditSectionDialog
-            section={editSection} />}
-          {editExercise && <EditExerciseDialog
-            exercise={editExercise}
-            open={!!editExercise}
-            validateExerciseName={validateExerciseName}
-            submit={contextValue.dispatch.save} />}
-          <ConfirmationDialog open={contextValue.state.confirmOpen} exercise={state.activeExercise} deleteExercise={deleteExercise} setConfirmOpen={contextValue.dispatch.setConfirmOpen} />
-          <NotificationSnackBar open={state.snackBarOpen} handleHide={contextValue.dispatch.setConfirmOpen} />
-        </div>
-      </ExerciseContext.Provider>
-    // </AppContext.Provider>
+    <ExerciseContext.Provider value={[state, dispatch]}>
+      <div className={classes.App}>
+        <Clock canvasSide={100} />
+        <BottomNavTabs />
+        {editSection && <EditSectionDialog
+          section={editSection} />}
+        {editExercise && <EditExerciseDialog
+          exercise={editExercise}
+          open={!!editExercise}
+          validateExerciseName={validateExerciseName}
+          submit={save} />}
+        <ConfirmationDialog open={confirmOpen} exercise={state.activeExercise} deleteExercise={deleteExercise} setConfirmOpen={setConfirmOpen} />
+        <NotificationSnackBar open={state.snackBarOpen} handleHide={setConfirmOpen} />
+      </div>
+    </ExerciseContext.Provider>
   );
 }
+
 
 export default withStyles(styles)(App);
