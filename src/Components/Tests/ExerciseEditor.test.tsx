6843b5858cc8ac4b53b1fc964b4dc9d3163423ec
rutiini <rutiluz88@gmail.com>
2020-03-08 20:52:24 +0200

    updated tests

---
+++ src/Components/Tests/ExerciseEditor.test.tsx
@@ -0,0 +1,16 @@
+﻿import * as React from 'react';
+import ReactDOM from 'react-dom';
+import ExerciseEditor from '../dialogs/ExerciseEditor';
+import { emptyExercise } from '../Utils';
+
+it('ExerciseEditor renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ExerciseEditor
+    exercise={emptyExercise}
+    usedNames={[]}
+    updateExercise={() => void 0}
+    deleteExercise={() => void 0}
+    cancel={() => void 0}
+    />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
---
+++ src/Components/Tests/SectionEditor.test.tsx
@@ -0,0 +1,15 @@
+﻿import * as React from 'react';
+import ReactDOM from 'react-dom';
+import SectionEditor from '../dialogs/SectionEditor';
+import { emptySection } from '../Utils';
+
+it('SectionEditor renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<SectionEditor 
+        section={emptySection} 
+        updateSection={() => void 0}
+        deleteSection={() => void 0}
+        cancel={() => void 0}
+        />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
