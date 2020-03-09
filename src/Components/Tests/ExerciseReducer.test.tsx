9eb1ce917c8a588ba16df6ad159b2c287cfeab8f
rutiini <rutiluz88@gmail.com>
2020-03-10 01:13:28 +0200

    more testing

--- src/Components/Tests/ExerciseReducer.test.tsx
+++ src/Components/Tests/ExerciseReducer.test.tsx
@@ -1,6 +1,5 @@
-﻿import * as React from "react";
-import { ExerciseReducer, DefaultAppState, ActionType, IAction } from "../AppReducer/ExerciseReducer";
-import { emptySection } from "../Utils";
+﻿import { ExerciseReducer, DefaultAppState, ActionType, IAction } from "../AppReducer/ExerciseReducer";
+import { emptySection, emptyExercise } from "../Utils";
 
 const initialState = DefaultAppState;
 const reducer = ExerciseReducer;
@@ -10,5 +9,30 @@ test("Setting active section mutates state correctly",() => {
     expect(reducer(initialState, SetActiveSection)).toEqual({
         ...initialState,
         activeSection: emptySection
+    });
+});
+
+test("Setting edit section mutates state correctly",() => {
+    const SetActiveSection: IAction = { type: ActionType.SetEditSection, payload: emptySection };
+    expect(reducer(initialState, SetActiveSection)).toEqual({
+        ...initialState,
+        editSection: emptySection
+    });
+});
+
+
+test("Setting edit Exercise mutates state correctly",() => {
+    const SetActiveExercise: IAction = { type: ActionType.SetEditExercise, payload: emptyExercise };
+    expect(reducer(initialState, SetActiveExercise)).toEqual({
+        ...initialState,
+        editExercise: emptyExercise
+    });
+});
+
+test("Setting active Exercise mutates state correctly",() => {
+    const SetActiveExercise: IAction = { type: ActionType.SetActiveExercise, payload: emptyExercise };
+    expect(reducer(initialState, SetActiveExercise)).toEqual({
+        ...initialState,
+        activeExercise: emptyExercise
     });
 });
