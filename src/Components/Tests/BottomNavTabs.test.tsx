0a0cb2f4585382cebbe711ff24fa3b661630ae87
rutiini <rutiluz88@gmail.com>
2020-03-10 19:48:20 +0200

    added a couple of more render tests and tweaked coverage file settings

---
+++ src/Components/Tests/BottomNavTabs.test.tsx
@@ -0,0 +1,9 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import BottomNavTabs from "../tabs/BottomNavTabs";
+
+it("BottomNavTabs renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<BottomNavTabs />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
---
+++ src/Components/Tests/CompactSectionListItem.test.tsx
@@ -0,0 +1,14 @@
+﻿import * as React from "react";
+import ReactDOM from "react-dom";
+import CompactSectionListItem from "../CompactSectionListItem";
+import { emptySection } from "../Utils";
+
+function mock(): void {
+    // mock function
+}
+
+it("CompactSectionListItem renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<CompactSectionListItem section={emptySection} index={0} editSection={mock} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
