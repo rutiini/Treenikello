98d326bfb05e8395188d10e2dd6b0d6709ce31d3
rutiini <rutiluz88@gmail.com>
2020-03-14 16:54:32 +0200

    started refactoring the stopwatch UNFINISHED

--- src/Components/Clock.tsx
+++ src/Components/Clock.tsx
@@ -1,5 +1,5 @@
 import { createStyles, Theme, WithStyles, withStyles } from "@material-ui/core";
-import React, { FunctionComponent, useContext, useState, useMemo } from "react";
+import React, { FunctionComponent, useContext, useState, useEffect } from "react";
 import { IExercise, ISection } from "../DataInterfaces";
 import ExerciseContext from "./AppReducer/ExerciseContext";
 import ClockFace from "./ClockFace";
@@ -10,7 +10,7 @@ import {
     CycleTimerMode,
     MinuteInDegrees,
     TimeToDegrees,
-    useInterval
+    useInterval,
 } from "./Utils/ClockUtilities";
 
 interface IProps extends WithStyles<typeof styles> {
@@ -21,25 +21,25 @@ export enum TimerMode {
     Hidden,
     Ready,
     Running,
-    Finished
+    Finished,
 }
 
 const styles = (theme: Theme) =>
     createStyles({
         activeSection: {
-            opacity: 1
+            opacity: 1,
         },
         clockContainer: {
             height: "45vh",
             [theme.breakpoints.between("md", "xl")]: {
                 float: "left",
                 height: "99vh",
-                width: "49vw"
-            }
+                width: "49vw",
+            },
         },
         inactiveSection: {
-            opacity: 0.65
-        }
+            opacity: 0.65,
+        },
     });
 
 const centerCoordinate = 100 / 2;
@@ -51,34 +51,34 @@ const centerCoordinate = 100 / 2;
 const Clock: FunctionComponent<IProps> = (props: IProps) => {
     const [stopWatchSeconds, setStopWatchSeconds] = useState(0);
     const [timerMode, setTimerMode] = useState(TimerMode.Hidden);
-    const [stopWatch, setStopWatch] = useState<NodeJS.Timeout | null>(null);
     const [time, setTime] = useState(new Date());
 
     useInterval(() => setTime(new Date()), 1000);
 
     // use the context to get the shared state and dispatch instace
-    const [state] = useContext(ExerciseContext); // state gets reassigned this way. -> Do we need context to actually share the state?
+    const [state] = useContext(ExerciseContext);
 
     /**
      * Increases the stopWatchSeconds in state by one.
      */
     const updateStopwatch = React.useCallback(() => {
-        setStopWatchSeconds(seconds => seconds + 1);
+        setStopWatchSeconds((seconds) => seconds + 1);
     }, [setStopWatchSeconds]);
 
     // stopwatch should react to the timermode
     // TODO: refactor this to a use-effect which handles the stopwatch outside state.
-    useMemo(() => {
-        if (timerMode === TimerMode.Running && stopWatchSeconds === 0) {
-            setStopWatch(setInterval(updateStopwatch, 500));
+    useEffect(() => {
+        let timer: NodeJS.Timeout | null = null;
+        if (timerMode === TimerMode.Running && timer === null) {
+            timer = setInterval(updateStopwatch, 500);
         } else if (timerMode === TimerMode.Finished) {
-            if (stopWatch) {
-                clearInterval(stopWatch as NodeJS.Timeout);
+            if (timer) {
+                clearInterval(timer);
             }
         } else if (timerMode === TimerMode.Hidden) {
             setStopWatchSeconds(0);
         }
-    }, [timerMode]);
+    }, [timerMode, setStopWatchSeconds, updateStopwatch]);
 
     /** cycle on tap -> make visible -> start -> stop -> hide and reset */
     const cycleTimerFunctions = React.useCallback(() => {
@@ -89,7 +89,7 @@ const Clock: FunctionComponent<IProps> = (props: IProps) => {
 
     const sectionItems: ReadonlyArray<JSX.Element> = React.useMemo(
         () => updateFaceElements(state.activeExercise, state.activeSection, props.classes),
-        [state.activeExercise, state.activeSection, props.classes]
+        [state.activeExercise, state.activeSection, props.classes],
     );
     const { classes } = props;
     const clockData = new ClockData(time);
@@ -114,7 +114,7 @@ const Clock: FunctionComponent<IProps> = (props: IProps) => {
 function updateFaceElements(
     selectedExercise: IExercise,
     activeSection: ISection | null,
-    classes: Record<"activeSection" | "inactiveSection", string>
+    classes: Record<"activeSection" | "inactiveSection", string>,
 ): ReadonlyArray<JSX.Element> {
     // TODO: handle midnight problem?
     // in order to enable full lenght that exceeds hour we need to track the hour as well.
@@ -136,13 +136,15 @@ function updateFaceElements(
             const setupStartAngle = cumulativeAngle;
             const setupStopAngle = cumulativeAngle + sectionItem.setupTime * MinuteInDegrees;
 
-            const sectionStartAngle = setupStopAngle; // + sectionItem.setupTime * MinuteInDegrees; // !added setup
-            cumulativeAngle += (sectionItem.duration + sectionItem.setupTime) * MinuteInDegrees; // transform minutes to degrees
+            const sectionStartAngle = setupStopAngle;
+            // transform minutes to degrees
+            cumulativeAngle += (sectionItem.duration + sectionItem.setupTime) * MinuteInDegrees;
 
             // if the section ends before starting angle dont draw it. Also if start is over an hour away
             if (cumulativeAngle <= currentPosition || currentPosition < cumulativeAngle - CircleInDegrees) {
                 // "extend" last visible section by new minutes
-                // TODO: current position is probably not the correct variable here since it can be in the middle of a section..
+                // TODO: current position is probably not the correct
+                // variable here since it can be in the middle of a section..
                 stopDrawAngle = stopDrawAngle + currentPosition;
             } else {
                 if (cumulativeAngle > stopDrawAngle + startPosition) {
@@ -161,7 +163,7 @@ function updateFaceElements(
                         key={sectionItems.length}
                         color={"#d3d0da"}
                         className={sectionItem === activeSection ? classes.activeSection : classes.inactiveSection}
-                    />
+                    />,
                 );
                 // section arc
                 sectionItems.push(
@@ -175,7 +177,7 @@ function updateFaceElements(
                         key={sectionItems.length}
                         color={sectionItem.color}
                         className={sectionItem === activeSection ? classes.activeSection : classes.inactiveSection}
-                    />
+                    />,
                 );
                 // just check whether there is an active item set.
             }
